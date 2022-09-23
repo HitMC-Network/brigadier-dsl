eefb8e2fd02aae5edcd1d545b14ed03b393760a1
JvstvsHD <fanoe@mailbox.org>
2022-09-23 18:41:46 +0200

    Update gradle, brigadier and add publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.21"
     `java-library`
+    `maven-publish`
 }
 
 repositories {
@@ -9,6 +10,23 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    api("com.mojang:brigadier:1.0.17")
+    api("com.mojang:brigadier:1.0.18")
+}
+
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/HitMC-Network/brigadier-dsl")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
